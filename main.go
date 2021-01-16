03e3e28e30c93af8d9d90bac5aa4da3c10af3636
SovHustler <sovianum@gmail.com>
2021-01-16 20:16:23 +0300

    add dataset

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 
 	tables := structuring.GroupToTables(result)
 
-	if err := tables.Write("/tmp/hustle"); err != nil {
+	if err := tables.Write("dataset"); err != nil {
 		panic(err)
 	}
 }
