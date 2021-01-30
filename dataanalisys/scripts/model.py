7c2b325431e054b60dcecbf5d780b983157c3950
SovHustler <sovianum@gmail.com>
2021-01-30 23:19:52 +0300

    decrease test data

--- dataanalisys/scripts/model.py
+++ dataanalisys/scripts/model.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 
 def get_regression(data: pd.DataFrame) -> typing.Tuple[LassoCV, float]:
-    x_train, x_test, y_train, y_test = train_test_split(data[data.columns[:-1]], data[data.columns[-1]], test_size=0.33, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(data[data.columns[:-1]], data[data.columns[-1]], test_size=0.1, random_state=42)
 
     reg = LassoCV(cv=5, random_state=42).fit(x_train, y_train)
 
